038a8cb5ca02160a6da03fb89e6b036875cdefd6
jonathanbello123 <jonathanbellogabriel@gmail.com>
2024-03-25 14:46:03 -0300

    fin

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,27 +1,51 @@
+import React, { useState, useEffect } from "react";
 import JBPIC from "../../assets/Front/JB.png";
 import { FaLongArrowAltRight, FaTwitter, FaFacebook, FaInstagram, FaWhatsapp } from "react-icons/fa";
 import { Link } from "react-scroll";
 
-
 export const Home = () => {
+    const [typedText, setTypedText] = useState("");
+    const textToType = " Jonathan Bello";
+    const typingSpeed = 100;
+
+    useEffect(() => {
+        let currentIndex = 0;
+        const typingTimer = setInterval(() => {
+            if (currentIndex <= textToType.length) {
+                setTypedText(textToType.substring(0, currentIndex));
+                currentIndex++;
+            } else {
+                clearInterval(typingTimer);
+
+                document.getElementById("blinking-cursor").classList.add("blinking-cursor");
+            }
+        }, typingSpeed);
+
+        return () => clearInterval(typingTimer);
+    }, []);
+
     return (
-        <div className="h-[750px] mx-auto w-1/2 mt-52" name="home" >
+        <div className="h-[750px] mx-auto w-1/2 mt-52" name="home">
             <div className="flex gap-20 items-center">
                 <div className="hidden xl:flex">
                     <img src={JBPIC} alt="" />
                 </div>
                 <div className="flex flex-col gap-4">
                     <h1 className="text-white text-5xl">
-                        Hello, I'm <span className="text-[#19ddd6]">Jonathan Bello</span>
+                        Hello, I'm
+                        <span className="text-[#19ddd6]"> {typedText}</span>
+
+                        <span id="blinking-cursor" className="text-white">|</span>
                     </h1>
                     <p className="text-lg text-white">
                         a <span className="text-[#19ddd6]">Front-End Developer</span> from Argentina, Buenos Aires.
                     </p>
                     <p className="text-lg text-white">
-                        I'm passionate about creating seamless and engaging digital experiences. Let's connect and bring your web projects to life!
+                        I'm passionate about creating seamless and engaging digital experiences. Let's connect and bring your web
+                        projects to life!
                     </p>
                     <div>
-                        <Link to="contact" smooth={true} duration={800} >
+                        <Link to="contact" smooth={true} duration={800}>
                             <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#19ddd6] hover:border-[#19ddd6] hover:text-black ">
                                 Contact me
                                 <span className="group-hover:rotate-90 duration-300">
@@ -29,7 +53,6 @@ export const Home = () => {
                                 </span>
                             </button>
                         </Link>
-
                     </div>
                     <div className="text-white">
                         <h2 className="text-2xl font-bold mb-2">Connect with me</h2>
@@ -51,6 +74,5 @@ export const Home = () => {
                 </div>
             </div>
         </div>
-
     );
 };
--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -6,14 +6,14 @@ import { ProjectCard } from "./ProjectCard"
 
 export const Projects = () => {
     return (
-        <div className="mb-12 h-full bg-black text-white" name="projects">
+        <div className=" mb-12 h-full bg-black text-white" name="projects">
             <div className="max-w-[1000px] mx-auto p4 flex flex-col justify-center w-full h-full">
                 <div className="items-center flex flex-col xl:items-start">
                     <p className="text-4xl font-bold inline border-b-4 border-[#19ddd6]">Projects</p>
                     <p className="py-4">These are some of my projects<span className="text-[#19ddd6] text-2xl">.</span></p>
                 </div>
             </div>
-            <div className="w-[50%] mx-auto grid grid-cols-1 xl:grid-cols-2 gap-4 text-center py-8">
+            <div className="w-[50%] mx-auto grid grid-cols-1 xl:grid-cols-2 gap-8 text-center py-8">
 
                 <ProjectCard
                     title="Fake Store"
