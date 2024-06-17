4011d487ff484bab49c69969a8498c06f0d656ba
jonathanbello123 <jonathanbellogabriel@gmail.com>
2024-06-17 15:33:16 -0300

    fin

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -4,7 +4,7 @@ export const Contact = () => {
             <form className="flex flex-col w-[65%] ">
                 <div className="pb-8 items-center flex flex-col xl:items-start">
                     <p className="text-4xl font-bold inline border-b-4 border-[#19ddd6] text-white">Contact </p>
-                    <p className="py-4 text-white">so we can get to know each other better!<span className="text-[#19ddd6] text-2xl">.</span></p>
+                    <p className="text-center py-4 text-white">so we can get to know each other better!<span className="text-[#19ddd6] text-2xl">.</span></p>
                 </div>
                 <input className="bg-gray-300 p-3 mb-4 rounded-lg" placeholder="Name" name="name" type="text" />
                 <input className="bg-gray-300 p-3 mb-4 rounded-lg" type="email" placeholder="Email" name="email" />
